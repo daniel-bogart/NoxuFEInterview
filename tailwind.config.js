84a27e5e25d369d564db2f63a9feb38346cf41d6
Daniel Bogart <75914647+daniel-bogart@users.noreply.github.com>
2024-04-15 15:36:49 -0700

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    transparent: "transparent",
+    current: "currentColor",
+    white: "#ffffff",
+    red: "#DC2626",
+    orange: "#F0671A",
+  },
+  plugins: [],
+};
